12bf2422e91ae0b19aee4514131e4ff498ce21ce
david <david.m.niu@gmail.com>
2023-04-18 18:32:29 -0400

    fuck

---
+++ bin/rps-cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { rps } from "../lib/rpsls.js";
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2))
+if (args.h || args.help) {
+	console.log(rps("help"));
+} else if (args.r || args.rules) {
+	console.log(rps("rules"));
+} else if (args._.length === 0) {
+	console.log(rps());
+} else {
+	console.log(rps(args._[0]));
+}
---
+++ bin/rpsls-cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { rpsls } from "../lib/rpsls.js";
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2))
+if (args.h || args.help) {
+	console.log(rpsls("help"));
+} else if (args.r || args.rules) {
+	console.log(rpsls("rules"));
+} else if (args._.length === 0) {
+	console.log(rpsls());
+} else {
+	console.log(rpsls(args._[0]));
+}
